677d1ada7a955f900e46d172956fd10edbd7dce2
Md Liton Mahmud <litonmahmud0023@gmail.com>
2024-01-13 01:03:44 +0600

    set image router and upload image in databse from client side

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, Db, ObjectId } = require('mongodb');
 const app = express();
+const fileupload = require('express-fileupload');
+const multer = require('multer');
+const mongoose =require('mongoose');
 require('dotenv').config();
 const cors =require('cors');
 const jwt =require('jsonwebtoken');
 const port =process.env.PORT || 5000;
 
+// app.use(
+//     fileupload({
+//       createParentPath: true,
+//     }),
+//   );
 
 // medile were
 
 app.use(cors());
 app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
 
 //verify jwt access token
@@ -40,6 +49,35 @@ const verifyJwt=(req,res,next)=>{
 // mongoDb connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bzjru.mongodb.net/?retryWrites=true&w=majority`;
 
+// mongoos connect
+mongoose.connect(uri,{ useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// Define a mongoose schema for your collection
+const productSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    imageUrl: String,
+  });
+  
+  const Product = mongoose.model('Product', productSchema);
+  
+  // Set up Multer to handle file uploads
+  const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'upload/');
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  });
+  
+  const upload = multer({ storage: storage }).single('image');
+  
+
+
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -56,6 +94,8 @@ async function run() {
     console.log('collcet mongo db')
 
     const usersCollection = client.db("bookShop").collection("users");
+    const booksCollection = client.db("bookShop").collection("books");
+    const imageCollection = client.db("bookShop").collection("uploads");
 
 
 
@@ -120,6 +160,61 @@ async function run() {
         const user = await usersCollection.findOne(query);
         const result ={admin: user?.role ==="admin"}
         console.log('admin:',result)
+        res.send(result);
+    })
+
+    // image apis
+
+    app.get('/uploads',async(req,res)=>{
+        const result = await imageCollection.find().toArray()
+        res.send(result);
+    })
+
+
+
+    // Handle image upload
+app.post('/uploads', async (req, res) => {
+
+    upload(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+          // A Multer error occurred when uploading.
+          return res.status(500).json({ success: false, message: 'Multer Error', error: err.message });
+        } else if (err) {
+          // An unknown error occurred when uploading.
+          return res.status(500).json({ success: false, message: 'Unknown Error', error: err.message });
+        }
+    
+        // No errors, continue processing.
+    
+        const { name, description } = req.body;
+    
+        // Save the image metadata in MongoDB
+        const product = new Product({
+          name: name,
+          description: description,
+          imageUrl: `/upload/${req.file.filename}`, // Assuming 'uploads' is a public directory
+        });
+    
+        product.save()
+          .then(() => {
+            res.json({ success: true,product, message: 'Image uploaded and metadata saved successfully.' });
+          })
+          .catch(error => {
+            console.error(error);
+            res.status(500).json({ success: false, message: 'Error saving to MongoDB' });
+          });
+      });
+  });
+
+    app.get('/books',async(req,res)=>{
+        const result =await booksCollection.find().toArray();
+        res.send(result);
+    })
+
+
+    app.post('/books',verifyJwt,verifyAdmin,async(req,res)=>{
+        const newBook =req.body;
+        const result =await booksCollection.insertOne(newBook);
         res.send(result);
     })
 
